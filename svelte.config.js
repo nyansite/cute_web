f99d2203b8683c8cb89364f0b1f8c9340d141c6a
tksmly <tksmly@gmail.com>
2023-06-11 02:09:40 +0800

    图标的压缩

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ export default {
             pages: 'build',
             assets: 'build',
             fallback: undefined,
-            precompress: true, //好麻烦啊,摸了
+            precompress: true,
             strict: true
         })
     }
