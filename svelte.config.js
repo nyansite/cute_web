3dc4b292a9439e6ce3cc2a35bbc08f59b4929d07
tksmly <tksmly@gmail.com>
2023-06-10 13:28:02 +0800

    压缩

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ export default {
             pages: 'build',
             assets: 'build',
             fallback: undefined,
-            precompress: false,
+            precompress: true,
             strict: true
         })
     }
