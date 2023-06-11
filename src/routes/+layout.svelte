e56faeb8d7c36ea0b8b42bc455d4427cfeeae336
tksmly <tksmly@gmail.com>
2023-06-12 05:28:16 +0800

    主题更改

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
-<nav id="Navbar">
+<script>
+  import { onMount } from "svelte";
+
+  let navbar_bg = "#333"; //默认主题颜色
+  onMount(() => {
+    // navbar_bg = "#7F583F"; //更改颜色
+  });
+</script>
+
+<nav id="Navbar" style="--navbar_bg: {navbar_bg}">
   <a rel="external" href="/">摸鱼</a>
   <a href="/">摸鱼</a>
   <a href="/">摸鱼</a>
 </nav>
-<slot />
+<slot  />
 
 <style lang="less">
-  @import "../theme/black.less";
+  @import "../theme.less";
 </style>
