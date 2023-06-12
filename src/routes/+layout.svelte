82a96f16d96821815640fe286656e02a55faeaff
tksmly <tksmly@gmail.com>
2023-06-13 07:27:25 +0800

    主题更改

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,29 @@
 <script>
   import { onMount } from "svelte";
-
-  let navbar_bg = "#333"; //默认主题颜色
+  import jsCookie from "js-cookie";
+  export let 导航栏背景 = "#333"; //我草
   onMount(() => {
-    // navbar_bg = "#7F583F"; //更改颜色
+    // navbar_bg = "#7F583F"; 
+    let theme = jsCookie.get("theme");
+    if (theme == null) {
+      theme = "black";
+    }
+    jsCookie.set("theme", theme, { expires: 60, path: "/" }); //单位天,chrome上限400,所以还要续
+    switch (theme) {
+      case "black":
+        导航栏背景 = "#333";
+        break;
+    }
+    console.log(theme);
   });
 </script>
 
-<nav id="Navbar" style="--navbar_bg: {navbar_bg}">
+<nav id="Navbar" style="--navbar_bg: {导航栏背景}">
   <a rel="external" href="/">摸鱼</a>
   <a href="/">摸鱼</a>
   <a href="/">摸鱼</a>
 </nav>
-<slot  />
+<slot />
 
 <style lang="less">
   @import "../theme.less";
