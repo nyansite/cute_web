f1975f558cf99e6412d6467ac0184da3a8d99c5b
tksmly <tksmly@gmail.com>
2023-06-13 15:01:27 +0800

    导航栏置顶

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,33 @@
   import { onMount } from "svelte";
   import jsCookie from "js-cookie";
   export let 导航栏背景 = "#333"; //我草
-  onMount(() => {
-    // navbar_bg = "#7F583F"; 
-    let theme = jsCookie.get("theme");
-    if (theme == null) {
-      theme = "black";
-    }
-    jsCookie.set("theme", theme, { expires: 60, path: "/" }); //单位天,chrome上限400,所以还要续
-    switch (theme) {
-      case "black":
-        导航栏背景 = "#333";
-        break;
-    }
-    console.log(theme);
-  });
+  export let 按钮背景 = "#333"; //我草
+
+  // navbar_bg = "#7F583F";
+  let theme = jsCookie.get("theme");
+  if (theme == null) {
+    theme = "咲夜白";
+  }
+  jsCookie.set("theme", theme, { expires: 114, path: "/" }); //单位天,chrome上限400,所以还要续,firefox超过限制不保存
+  switch (theme) {
+    case "咲夜白":
+      导航栏背景 = "#D3D3D3";
+      按钮背景 = "#bfbfbf";
+
+      break;
+    case "baka蓝":
+      导航栏背景 = "CBF1F5";
+      break;
+  }
+  console.log(theme);
 </script>
 
-<nav id="Navbar" style="--navbar_bg: {导航栏背景}">
+<nav id="Navbar" style="--navbar_bg: {导航栏背景};--button_bg:{按钮背景}">
   <a rel="external" href="/">摸鱼</a>
   <a href="/">摸鱼</a>
   <a href="/">摸鱼</a>
 </nav>
+<br />
 <slot />
 
 <style lang="less">
