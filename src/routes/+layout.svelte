e07ee144ceab43123062ccd8e60fd23d28c62bd9
tksmly <tksmly@gmail.com>
2023-06-16 18:07:28 +0800

    首页图标不位移

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,11 +26,11 @@
   id="Navbar"
   style="--navbar_bg: {navbar_bg};--button_bg:{button_bg};--navbar_text{navbar_text}"
 >
-<div id="logo"><img src="/house-solid.svg" alt="logo" /></div>
-  
-  <a rel="external" href="/">摸鱼</a>
-  <a href="/">摸鱼</a>
-  <a href="/">摸鱼</a>
+  <a href="/" id="home"><img src="/house-solid.svg" alt="home" /></a>
+
+  <a class="nav_button" rel="external" href="/">摸鱼</a>
+  <a class="nav_button" href="/">摸鱼</a>
+  <a class="nav_button" href="/">摸鱼</a>
 </nav>
 <br />
 <slot />
