97a94ae63bd99fba58f6eb875bb8a7c7458d6f03
tksmly <tksmly@gmail.com>
2023-06-19 13:49:28 +0800

    预加载备用ico

--- static_in/compress.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-mkdir ../static
-for file in ./*.*
-    if test $file -ef (status filename);
-        continue
-    end
-    brotli $file -o ../static/{$file}.br -f
-    cp $file ../static/{$file}
-end
